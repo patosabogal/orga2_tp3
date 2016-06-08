95065f20db2ff97b64bdc305776e368997049bfd
patricio sabogal <psabogal@ws8.labo5>
2016-06-07 21:55:51 -0300

    empezado procmno indice

--- src/sched.c
+++ src/sched.c
@@ -7,25 +7,40 @@
 
 #include "sched.h"
 
-// id _proximo_tipo(id _actual){
-// 	switch (_actual){
-// 		case A:
-// 			return B;
-// 		case B:
-// 			return H;
-// 		case H:
-// 			return A;
-// 	}
-// }
+id _proximo_tipo(id _actual){
+	switch (_actual){
+		case A:
+			return B;
+		case B:
+			return H;
+		case H:
+			return A;
+	}
+
+	return 0;
+}
+
+unsigned int _no_tiene_vivos(id _id){
+	return FALSE;
+}
 
 
 unsigned short sched_proximo_indice() {
-	// id _actual = GAME.corriendo;
+	id _actual = GAME.corriendo;
 
-	// id _prox;
-	// do while () = _proximo_tipo(_actual);
+	id _prox = _proximo_tipo(_actual);
+	while (_no_tiene_vivos(_prox) && _prox != _actual ) {
+		_prox = _proximo_tipo(_prox);
+	}
 
+	if (_prox == _actual && _no_tiene_vivos(_actual)){
+		// todos muertos
+		return 0;
+	} else {
+		// no estan todos muertos
+		// ACA TE DEVUELVO EL SELECTOR DE LA TAREA
+		return 0;
+	}
 
-
-	return 0;
+	return 0;	
 }
